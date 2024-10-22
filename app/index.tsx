bda64b006b00c95a98a0f1086d4ca57cd93fb1ed
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-10-22 14:24:45 +0300

    expo router setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+
+import { View,Text } from "react-native"
+
+function HomePage() {
+  return (
+    <View>
+        <Text style={{color:"red"}}>HomePage</Text>
+        </View>
+  )
+}
+
+export default HomePage
---
+++ components/ProductListItem.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function ProductListItem() {
+  return (
+    <View>
+      <Text>ProductListItem</Text>
+    </View>
+  )
+}
